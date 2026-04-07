ae96181e136baf25bbf6c0b40c0fb165ed98842f
yossq236 <yf0659@gmail.com>
2026-04-07 23:06:27 +0900

    Add debug dump for internal state

--- nodes.py
+++ nodes.py
@@ -31,16 +31,16 @@ class SDXLCLIPTextEncodeNode(io.ComfyNode):
     @classmethod
     def execute(cls, clip, width, height, crop_w, crop_h, target_width, target_height, positive, negative, use_break) -> io.NodeOutput:
         return io.NodeOutput(
-            cls.clip_encode(clip, width, height, crop_w, crop_h, target_width, target_height, positive, use_break),
-            cls.clip_encode(clip, width, height, crop_w, crop_h, target_width, target_height, negative, use_break),
+            cls.clip_encode(clip, width, height, crop_w, crop_h, target_width, target_height, positive, use_break, True),
+            cls.clip_encode(clip, width, height, crop_w, crop_h, target_width, target_height, negative, use_break, False),
         )
 
     @classmethod
-    def clip_encode(cls, clip: CLIP, width: int, height: int, crop_w: int, crop_h: int, target_width: int, target_height: int, text: str, use_break: bool) -> any:
+    def clip_encode(cls, clip: CLIP, width: int, height: int, crop_w: int, crop_h: int, target_width: int, target_height: int, text: str, use_break: bool, dump: bool) -> any:
         if use_break:
             break_list = re.split(r"[ ]*BREAK[ ]*,?", text)
             for i, v in enumerate(break_list):
-                print(f"Processing chunk {i+1}/{len(break_list)}: '{v.strip().replace('\n', '\\n')}'")
+                # print(f"Processing chunk {i+1}/{len(break_list)}: '{v.strip().replace('\n', '\\n')}'")
                 if i == 0:
                     tokens = clip.tokenize(v.strip())
                 else:
@@ -49,14 +49,38 @@ class SDXLCLIPTextEncodeNode(io.ComfyNode):
                     tokens["l"] += tokens_add["l"]
         else:
             tokens = clip.tokenize(text)
-            # tokens["l"] = clip.tokenize(text)["l"]
         if len(tokens["l"]) != len(tokens["g"]):
             empty = clip.tokenize("")
             while len(tokens["l"]) < len(tokens["g"]):
                 tokens["l"] += empty["l"]
             while len(tokens["l"]) > len(tokens["g"]):
                 tokens["g"] += empty["g"]
+        if dump:
+            cls.dump_tokens("text_g", tokens["g"], {v: k for k, v in clip.tokenizer.clip_g.tokenizer.get_vocab().items()})
+            cls.dump_tokens("text_l", tokens["l"], {v: k for k, v in clip.tokenizer.clip_l.tokenizer.get_vocab().items()})
         return clip.encode_from_tokens_scheduled(tokens, add_dict={"width": width, "height": height, "crop_w": crop_w, "crop_h": crop_h, "target_width": target_width, "target_height": target_height})
+    
+    @classmethod
+    def dump_tokens(cls, title, tokens, id_to_token):
+        print(f"# {title}")
+        num_chunks = len(tokens)
+        for i, chunk in enumerate(tokens):
+            token_ids = [item[0] for item in chunk]
+            token_strings = [id_to_token.get(token_id, "[UNK]") for token_id in token_ids]
+            print(f" - chunk {i+1}/{num_chunks}: {cls.get_chunk_string(token_strings)}")
+    
+    @classmethod
+    def get_chunk_string(cls, words):
+        chunk = []
+        in_chunk = False
+        for word in words:
+            if word == "<|startoftext|>":
+                in_chunk = True
+            elif word == "<|endoftext|>":
+                in_chunk = False
+            elif in_chunk:
+                chunk.append(word.replace("</w>", "|"))
+        return ' '.join(chunk)
 
 class SDXLClipTextTokenDumpNode(io.ComfyNode):
 
